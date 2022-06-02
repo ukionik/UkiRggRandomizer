1812e3eb80d05aa339f61e2df3c21f268fa07f2a
Vyzhanov Andrey <ukionik@gmail.com>
2022-06-02 22:02:26 +0300

    Добавлен вывод страницы в рандомайзер. Поднят http сервер

--- UkiRggRandomizer/UkiRggRandomizer/App.xaml.cs
+++ UkiRggRandomizer/UkiRggRandomizer/App.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using GenHTTP.Engine;
+using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Practices;
+using GenHTTP.Modules.StaticWebsites;
 
 namespace UkiRggRandomizer
 {
@@ -13,5 +18,18 @@ namespace UkiRggRandomizer
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var tree = ResourceTree.FromDirectory("www/");
+            var app = StaticWebsite.From(tree);
+            
+            var host = Host.Create()
+                .Handler(app)
+                .Defaults()
+                .Port(18234)
+                .Start();
+            
+            
+        }
     }
 }
--- ukirggrandomizer.web/vue.config.js
+++ ukirggrandomizer.web/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: '../UkiRggRandomizer/UkiRggRandomizer/www',
   transpileDependencies: true
 })
