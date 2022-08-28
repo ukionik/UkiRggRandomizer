9980e2e0c06a51b26c861ffbd35f69584770c961
Vyzhanov Andrey <ukionik@gmail.com>
2022-08-28 15:30:52 +0300

    Добавлен стиль окна под Metro

--- UkiRandomizer/UkiRandomizer/MainWindow.xaml.cs
+++ UkiRandomizer/UkiRandomizer/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace UkiRandomizer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
