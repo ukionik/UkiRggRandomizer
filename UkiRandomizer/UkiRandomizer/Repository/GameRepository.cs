e323b11c00057dbcd40fcc58157ca845c4c0c71f
Vyzhanov Andrey <ukionik@gmail.com>
2023-07-01 18:59:46 +0300

    ---

---
+++ UkiRandomizer/UkiRandomizer/Repository/GameRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UkiRandomizer.Core;
+using UkiRandomizer.Model.Entity;
+using UkiRandomizer.Service;
+
+namespace UkiRandomizer.Repository;
+
+[Repository]
+public class GameRepository : IGameRepository
+{
+    private readonly IDbService _dbService;
+    private readonly PlatformRepository _platformRepository;
+    private readonly ISheetService _sheetService;
+
+    public GameRepository(ISheetService sheetService
+        , IDbService dbService
+        , PlatformRepository platformRepository)
+    {
+        _sheetService = sheetService;
+        _dbService = dbService;
+        _platformRepository = platformRepository;
+    }
+
+    public List<Game> Data { get; private set; }
+
+    public Task LoadAsync()
+    {
+        var platforms = _platformRepository.Data;
+
+        foreach (var platform in platforms)
+        {
+            
+        }
+
+        return null;
+    }
+}
